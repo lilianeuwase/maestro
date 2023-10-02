9437aa5295d54b73fcf6064f40cfb27059b148be
lilianeuwase <38309956+lilianeuwase@users.noreply.github.com>
2023-10-02 16:23:37 +0200

    2-Oct commit

--- src/components/about/intro.js
+++ src/components/about/intro.js
@@ -1,6 +1,6 @@
 export const Intro = () => {
   return (
-    <div class="dark max-w-[90rem] px-4 py-24 sm:px-6 lg:px-8 lg:py-32 mx-auto bg-black">
+    <div class="dark px-4 py-24 sm:px-6 lg:px-8 lg:py-32 mx-auto bg-black">
       {/* Tab Nav */}
       <div
         class="max-w-6xl mx-auto grid grid-cols-2 sm:grid-cols-4 gap-2 sm:gap-4"
@@ -24,15 +24,16 @@ export const Intro = () => {
               fill="currentColor"
               viewBox="0 0 16 16"
             >
-              <path d="M5.5 2A3.5 3.5 0 0 0 2 5.5v5A3.5 3.5 0 0 0 5.5 14h5a3.5 3.5 0 0 0 3.5-3.5V8a.5.5 0 0 1 1 0v2.5a4.5 4.5 0 0 1-4.5 4.5h-5A4.5 4.5 0 0 1 1 10.5v-5A4.5 4.5 0 0 1 5.5 1H8a.5.5 0 0 1 0 1H5.5z" />
-              <path d="M16 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+              <path d="M6 12.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5ZM3 8.062C3 6.76 4.235 5.765 5.53 5.886a26.58 26.58 0 0 0 4.94 0C11.765 5.765 13 6.76 13 8.062v1.157a.933.933 0 0 1-.765.935c-.845.147-2.34.346-4.235.346-1.895 0-3.39-.2-4.235-.346A.933.933 0 0 1 3 9.219V8.062Zm4.542-.827a.25.25 0 0 0-.217.068l-.92.9a24.767 24.767 0 0 1-1.871-.183.25.25 0 0 0-.068.495c.55.076 1.232.149 2.02.193a.25.25 0 0 0 .189-.071l.754-.736.847 1.71a.25.25 0 0 0 .404.062l.932-.97a25.286 25.286 0 0 0 1.922-.188.25.25 0 0 0-.068-.495c-.538.074-1.207.145-1.98.189a.25.25 0 0 0-.166.076l-.754.785-.842-1.7a.25.25 0 0 0-.182-.135Z" />
+              <path d="M8.5 1.866a1 1 0 1 0-1 0V3h-2A4.5 4.5 0 0 0 1 7.5V8a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1v1a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-1a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1v-.5A4.5 4.5 0 0 0 10.5 3h-2V1.866ZM14 7.5V13a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.5A3.5 3.5 0 0 1 5.5 4h5A3.5 3.5 0 0 1 14 7.5Z" />
             </svg>
             <span class="md:grow md:ml-5">
               <span class="hs-tab-active:text-customized-10 block font-semibold text-gray-800 dark:hs-tab-active:text-customized-10 dark:text-gray-200">
-              Depression
+                Depression
               </span>
               <span class="hidden lg:block mt-2 text-gray-800 dark:text-gray-200">
-              Depression can happen to anyone, regardless of sex, race, or age. 
+                Depression can happen to anyone, regardless of sex, race, or
+                age.
               </span>
             </span>
           </span>
@@ -62,10 +63,11 @@ export const Intro = () => {
             </svg>
             <span class="md:grow md:ml-5">
               <span class="hs-tab-active:text-customized-10 block font-semibold text-gray-800 dark:hs-tab-active:text-customized-10 dark:text-gray-200">
-              Anxiety
+                Anxiety
               </span>
               <span class="hidden lg:block mt-2 text-gray-800 dark:text-gray-200">
-              Persistent worrying and racing thoughts are part of the diagnostic criteria for anxiety.
+                Persistent worrying and racing thoughts are part of the
+                diagnostic criteria for anxiety.
               </span>
             </span>
           </span>
@@ -92,10 +94,10 @@ export const Intro = () => {
             </svg>
             <span class="md:grow md:ml-5">
               <span class="hs-tab-active:text-customized-10 block font-semibold text-gray-800 dark:hs-tab-active:text-customized-10 dark:text-gray-200">
-              Substance Abuse
+                Substance Abuse
               </span>
               <span class="hidden lg:block mt-2 text-gray-800 dark:text-gray-200">
-              Addiction can happen at any age.
+                Addiction can happen at any age.
               </span>
             </span>
           </span>
@@ -103,9 +105,9 @@ export const Intro = () => {
         <button
           type="button"
           class="hs-tab-active:bg-gray-100 hs-tab-active:hover:border-transparent text-center md:text-left hover:bg-gray-100 p-3 md:p-5 rounded-xl dark:hs-tab-active:bg-white/[.05] dark:hover:bg-gray-700"
-          id="tabs-with-card-item-3"
-          data-hs-tab="#tabs-with-card-3"
-          aria-controls="tabs-with-card-3"
+          id="tabs-with-card-item-4"
+          data-hs-tab="#tabs-with-card-4"
+          aria-controls="tabs-with-card-4"
           role="tab"
         >
           <span class="md:flex">
@@ -117,14 +119,17 @@ export const Intro = () => {
               fill="currentColor"
               viewBox="0 0 16 16"
             >
-              <path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641l2.5-8.5zM6.374 1 4.168 8.5H7.5a.5.5 0 0 1 .478.647L6.78 13.04 11.478 7H8a.5.5 0 0 1-.474-.658L9.306 1H6.374z" />
+              <path
+                fill-rule="evenodd"
+                d="m8 2.42-.717-.737c-1.13-1.161-3.243-.777-4.01.72-.35.685-.451 1.707.236 3.062C4.16 6.753 5.52 8.32 8 10.042c2.479-1.723 3.839-3.29 4.491-4.577.687-1.355.587-2.377.236-3.061-.767-1.498-2.88-1.882-4.01-.721L8 2.42Zm-.49 8.5c-10.78-7.44-3-13.155.359-10.063.045.041.089.084.132.129.043-.045.087-.088.132-.129 3.36-3.092 11.137 2.624.357 10.063l.235.468a.25.25 0 1 1-.448.224l-.008-.017c.008.11.02.202.037.29.054.27.161.488.419 1.003.288.578.235 1.15.076 1.629-.157.469-.422.867-.588 1.115l-.004.007a.25.25 0 1 1-.416-.278c.168-.252.4-.6.533-1.003.133-.396.163-.824-.049-1.246l-.013-.028c-.24-.48-.38-.758-.448-1.102a3.177 3.177 0 0 1-.052-.45l-.04.08a.25.25 0 1 1-.447-.224l.235-.468ZM6.013 2.06c-.649-.18-1.483.083-1.85.798-.131.258-.245.689-.08 1.335.063.244.414.198.487-.043.21-.697.627-1.447 1.359-1.692.217-.073.304-.337.084-.398Z"
+              />
             </svg>
             <span class="md:grow md:ml-5">
               <span class="hs-tab-active:text-customized-10 block font-semibold text-gray-800 dark:hs-tab-active:text-customized-10 dark:text-gray-200">
-              LGBTQ+ Individuals
+                LGBTQ+ Individuals
               </span>
               <span class="hidden lg:block mt-2 text-gray-800 dark:text-gray-200">
-              Many LGBTQ+ people experience mental health struggles.
+                Many LGBTQ+ people experience mental health struggles.
               </span>
             </span>
           </span>
@@ -208,7 +213,7 @@ export const Intro = () => {
                   <span class="w-2 h-2 bg-gray-700 rounded-full"></span>
                 </div>
                 <div class="flex justify-center items-center w-full h-full bg-gray-700 text-[.25rem] text-gray-200 rounded-sm sm:text-[.5rem]">
-                www.maestromindlab.org
+                  www.maestromindlab.org
                 </div>
               </div>
 
@@ -238,7 +243,7 @@ export const Intro = () => {
               <div class="p-1.5 bg-gray-100 rounded-3xl shadow-[0_2.75rem_5.5rem_-3.5rem_rgb(45_55_75_/_20%),_0_2rem_4rem_-2rem_rgb(45_55_75_/_30%),_inset_0_-0.1875rem_0.3125rem_0_rgb(45_55_75_/_20%)] dark:bg-gray-700 dark:shadow-[0_2.75rem_5.5rem_-3.5rem_rgb(0_0_0_/_20%),_0_2rem_4rem_-2rem_rgb(0_0_0_/_30%),_inset_0_-0.1875rem_0.3125rem_0_rgb(0_0_0_/_20%)]">
                 <img
                   class="max-w-full h-auto rounded-[1.25rem]"
-                  src="../assets/img/mockups/img13.jpg"
+                  src={require("../../assets/SubstancePhone.png")}
                   alt="Image Description"
                 />
               </div>
@@ -254,14 +259,59 @@ export const Intro = () => {
                   <span class="w-2 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></span>
                 </div>
                 <div class="flex justify-center items-center w-full h-full bg-gray-200 text-[.25rem] text-gray-800 rounded-sm sm:text-[.5rem] dark:bg-gray-700 dark:text-gray-200">
-                www.maestromindlab.org
+                  www.maestromindlab.org
                 </div>
               </div>
 
               <div class="bg-gray-800 rounded-b-lg">
                 <img
                   class="max-w-full h-auto rounded-b-lg"
-                  src="../assets/img/mockups/img12.jpg"
+                  src={require("../../assets/SubstancePc.png")}
+                  alt="Image Description"
+                />
+              </div>
+            </figure>
+            {/* End Browser Device */}
+          </div>
+          {/* End Devices */}
+        </div>
+        <div
+          id="tabs-with-card-4"
+          class="hidden"
+          role="tabpanel"
+          aria-labelledby="tabs-with-card-item-4"
+        >
+          {/* Devices */}
+          <div class="max-w-[1140px] lg:pb-32 relative">
+            {/* Mobile Device */}
+            <figure class="hidden absolute bottom-0 left-0 z-[2] max-w-full w-60 h-auto mb-20 ml-20 lg:block">
+              <div class="p-1.5 bg-gray-100 rounded-3xl shadow-[0_2.75rem_5.5rem_-3.5rem_rgb(45_55_75_/_20%),_0_2rem_4rem_-2rem_rgb(45_55_75_/_30%),_inset_0_-0.1875rem_0.3125rem_0_rgb(45_55_75_/_20%)] dark:bg-gray-700 dark:shadow-[0_2.75rem_5.5rem_-3.5rem_rgb(0_0_0_/_20%),_0_2rem_4rem_-2rem_rgb(0_0_0_/_30%),_inset_0_-0.1875rem_0.3125rem_0_rgb(0_0_0_/_20%)]">
+                <img
+                  class="max-w-full h-auto rounded-[1.25rem]"
+                  src={require("../../assets/lgbtqPhone.png")}
+                  alt="Image Description"
+                />
+              </div>
+            </figure>
+            {/* End Mobile Device */}
+
+            {/* Browser Device */}
+            <figure class="ml-auto mr-20 relative z-[1] max-w-full w-[50rem] h-auto rounded-b-lg shadow-[0_2.75rem_3.5rem_-2rem_rgb(45_55_75_/_20%),_0_0_5rem_-2rem_rgb(45_55_75_/_15%)] dark:shadow-[0_2.75rem_3.5rem_-2rem_rgb(0_0_0_/_20%),_0_0_5rem_-2rem_rgb(0_0_0_/_15%)]">
+              <div class="relative flex items-center max-w-[50rem] bg-white border-b border-gray-100 rounded-t-lg py-2 px-24 dark:bg-gray-800 dark:border-gray-700">
+                <div class="flex space-x-1 absolute top-2/4 left-4 -translate-y-1">
+                  <span class="w-2 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></span>
+                  <span class="w-2 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></span>
+                  <span class="w-2 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></span>
+                </div>
+                <div class="flex justify-center items-center w-full h-full bg-gray-200 text-[.25rem] text-gray-800 rounded-sm sm:text-[.5rem] dark:bg-gray-700 dark:text-gray-200">
+                  www.maestromindlab.org
+                </div>
+              </div>
+
+              <div class="bg-gray-800 rounded-b-lg">
+                <img
+                  class="max-w-full h-auto rounded-b-lg"
+                  src={require("../../assets/lgbtqPc.png")}
                   alt="Image Description"
                 />
               </div>
--- src/components/home/joinCommunity.js
+++ src/components/home/joinCommunity.js
@@ -1,6 +1,6 @@
 export const JoinCommunity = () => {
   return (
-    <div class="dark max-w-[90rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto bg-customized-12">
+    <div class="dark px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto bg-customized-12">
       <div class="relative p-6 md:p-16">
         <div class="relative z-10 lg:grid lg:grid-cols-12 lg:gap-16 lg:items-center">
           <div class="mb-10 lg:mb-0 lg:col-span-6 lg:col-start-8 lg:order-2">
@@ -30,8 +30,7 @@ export const JoinCommunity = () => {
                     fill="currentColor"
                     viewBox="0 0 16 16"
                   >
-                    <path d="M5.5 2A3.5 3.5 0 0 0 2 5.5v5A3.5 3.5 0 0 0 5.5 14h5a3.5 3.5 0 0 0 3.5-3.5V8a.5.5 0 0 1 1 0v2.5a4.5 4.5 0 0 1-4.5 4.5h-5A4.5 4.5 0 0 1 1 10.5v-5A4.5 4.5 0 0 1 5.5 1H8a.5.5 0 0 1 0 1H5.5z" />
-                    <path d="M16 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                    <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z" />
                   </svg>
                   <span class="grow ml-6">
                     <span class="block text-lg font-semibold hs-tab-active:text-blue-600 text-gray-800 dark:hs-tab-active:text-blue-500 dark:text-gray-200">
@@ -63,10 +62,7 @@ export const JoinCommunity = () => {
                     fill="currentColor"
                     viewBox="0 0 16 16"
                   >
-                    <path
-                      fill-rule="evenodd"
-                      d="M0 0h1v15h15v1H0V0Zm14.817 3.113a.5.5 0 0 1 .07.704l-4.5 5.5a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61 4.15-5.073a.5.5 0 0 1 .704-.07Z"
-                    />
+                    <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
                   </svg>
                   <span class="grow ml-6">
                     <span class="block text-lg font-semibold hs-tab-active:text-blue-600 text-gray-800 dark:hs-tab-active:text-blue-500 dark:text-gray-200">
@@ -99,7 +95,7 @@ export const JoinCommunity = () => {
                     fill="currentColor"
                     viewBox="0 0 16 16"
                   >
-                    <path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641l2.5-8.5zM6.374 1 4.168 8.5H7.5a.5.5 0 0 1 .478.647L6.78 13.04 11.478 7H8a.5.5 0 0 1-.474-.658L9.306 1H6.374z" />
+                    <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935zM3.504 1c.007.517.026 1.006.056 1.469.13 2.028.457 3.546.87 4.667C5.294 9.48 6.484 10 7 10a.5.5 0 0 1 .5.5v2.61a1 1 0 0 1-.757.97l-1.426.356a.5.5 0 0 0-.179.085L4.5 15h7l-.638-.479a.501.501 0 0 0-.18-.085l-1.425-.356a1 1 0 0 1-.757-.97V10.5A.5.5 0 0 1 9 10c.516 0 1.706-.52 2.57-2.864.413-1.12.74-2.64.87-4.667.03-.463.049-.952.056-1.469H3.504z" />
                   </svg>
                   <span class="grow ml-6">
                     <span class="block text-lg font-semibold hs-tab-active:text-blue-600 text-gray-800 dark:hs-tab-active:text-blue-500 dark:text-gray-200">
@@ -118,7 +114,10 @@ export const JoinCommunity = () => {
                 </span>
               </button>
             </div>
-            <p className="mt-4 text-xl text-gray-800 font-bold sm:text-lg dark:text-customized-7 text-center">Welcome to Maestro Mind – where every step forward is a step toward a life of purpose, joy, and inner peace.</p>
+            <p className="mt-4 text-xl text-gray-800 font-bold sm:text-lg dark:text-customized-7 text-center">
+              Welcome to Maestro Mind – where every step forward is a step
+              toward a life of purpose, joy, and inner peace.
+            </p>
           </div>
 
           <div class="lg:col-span-6">
@@ -131,7 +130,7 @@ export const JoinCommunity = () => {
                 >
                   <img
                     class="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/[.2]"
-                    src="https://images.unsplash.com/photo-1605629921711-2f6b00c6bbf4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&h=1220&q=80"
+                    src={require("../../assets/more.png")}
                     alt="Image Description"
                   />
                 </div>
@@ -143,8 +142,8 @@ export const JoinCommunity = () => {
                   aria-labelledby="tabs-with-card-item-2"
                 >
                   <img
-                    class="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/[.2]"
-                    src="https://images.unsplash.com/photo-1665686306574-1ace09918530?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&h=1220&q=80"
+                    class="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/[.2]" 
+                    src={require("../../assets/youbelong.png")}
                     alt="Image Description"
                   />
                 </div>
@@ -156,8 +155,8 @@ export const JoinCommunity = () => {
                   aria-labelledby="tabs-with-card-item-3"
                 >
                   <img
-                    class="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/[.2]"
-                    src="https://images.unsplash.com/photo-1598929213452-52d72f63e307?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&h=1220&q=80"
+                    class="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/[.2]" 
+                    src={require("../../assets/yourjourney.png")}
                     alt="Image Description"
                   />
                 </div>
