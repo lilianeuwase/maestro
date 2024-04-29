e734a2abd6db84625fb30cc021fe5af6bdb0f052
lilianeuwase <38309956+lilianeuwase@users.noreply.github.com>
2024-04-29 20:52:48 +0200

    git cache cleared

--- src/components/therapy/cards/cardsHeader.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { Avatar } from "react-lorem-ipsum";
-
-export const CardHeader = () => (
-  <div className="headerCard">
-    <span className="dateCard">CBT 4 STEPS</span> <br/>
-    <span className="h1Card">How CBT Works</span>
-  </div>
-);
